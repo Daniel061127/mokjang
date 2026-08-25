96109a6a9ceb2f48fcd4f29a73653d55bb005fd8
Daniel061127 <cky061127@naver.com>
2026-08-25 16:00:50 +0900

    git commit -m "소제목 수정"

--- src/main.ts
+++ src/main.ts
@@ -247,7 +247,7 @@ function renderHome() {
     header.className = 'header';
     header.innerHTML = `
         <h1>오늘의 말씀</h1>
-        <p>오늘도 하나님의 말씀으로 살아가는 빡다방 목장</p>
+        <p>오늘도 하나님의 말씀으로 살아가길 소망합니다!</p>
     `;
     container.appendChild(header);
 
